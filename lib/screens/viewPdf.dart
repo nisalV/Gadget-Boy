1f3af8e6133631eee086b7736fefeafbbf53a7b6
nisal <nisal.pathmila.perera@gmail.com>
2022-09-04 12:42:00 +0530

    changed PDF view library

--- lib/screens/viewPdf.dart
+++ lib/screens/viewPdf.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
+import 'package:flutter_cached_pdfview/flutter_cached_pdfview.dart';
 
 class ViewPDF extends StatefulWidget {
   final String pdfName,link;
@@ -17,11 +17,9 @@ class _PdfState extends State<ViewPDF> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SfPdfViewer.network(
-            widget.link,
-        )
-      ),
+      body: const PDF().fromUrl(widget.link,
+        placeholder: (double progress) => Center(child: Text('$progress %')),
+        errorWidget: (dynamic error) => Center(child: Text(error.toString())),),
     );
   }
 }
